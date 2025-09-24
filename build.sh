9df75cbad23c3e8f64af6ef77d6626cfcdee3af3
willjasen <brandon@willjasen.com>
2025-09-24 17:06:07 -0400

    use a script to build the plugin

---
+++ build.sh
@@ -0,0 +1,28 @@
+#!/bin/sh
+
+# This script builds the plugin and copies the output files into the associated plugin directory
+# After this build script is run, this plugin in Obisidian should disabled/enabled to refresh it
+
+# variables
+SOURCE_DIR="/Users/willjasen/Application Data/GitHub/obsidian-proxmox-notes";
+PLUGIN_DIR="/Users/willjasen/Library/Mobile Documents/iCloud~md~obsidian/Documents/willjasen/.obsidian/plugins/proxmox-notes";
+FILES=( "main.js" "manifest.json" "styles.css" );
+
+# Create the directory if needed
+if [ -d $PLUGIN_DIR ]; then 
+  echo "Plugin directory already exists.";
+else
+  echo "Creating plugin directory at $PLUGIN_DIR"; mkdir $PLUGIN_DIR;
+fi
+
+# Go to the source directory and build this plugin
+cd $SOURCE_DIR;
+npm i --silent; npm run build > /dev/null 2>&1;
+
+# Copy built files into the plugin directory
+for file in "${FILES[@]}"
+do
+  echo "Copying file: $file --> $PLUGIN_DIR/$file";
+  cp $SOURCE_DIR/$file $PLUGIN_DIR/$file;
+done
+echo "All files copied!";
